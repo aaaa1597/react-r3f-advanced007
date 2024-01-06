335a0eab5c75bc305cd9a9f47866d3bf784b75b8
aaaa1597 <tokushi2011@gmail.com>
2024-01-06 20:04:34 +0900

    初回コミット2

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import React, {useRef} from 'react';
+import React, { useRef, Suspense} from 'react';
 import './App.css';
-import { Canvas, useFrame, MeshProps  } from '@react-three/fiber'
+import { Canvas, useLoader, useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 import { OrbitControls, Environment } from '@react-three/drei'
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
-const Box = (props: MeshProps) => {
-  const ref = useRef<THREE.Mesh>(null!)
+const TheModel = () => {
+  const { scene, animations } = useLoader(GLTFLoader, "/animate-bones.glb");
+  const mixer = new THREE.AnimationMixer(scene);
+  mixer.clipAction(animations[0]).play();
+  mixer.clipAction(animations[1]).play();
 
-  useFrame((_, delta) => {
-    if( !ref.current) return
-    ref.current.rotation.x += 1 * delta
-    ref.current.rotation.y += 0.5 * delta
-  })
+  useFrame((state, delta) => {
+    mixer.update(delta);
+  });
 
   return (
-    <mesh {...props} ref={ref}>
-      <boxGeometry />
-      <meshNormalMaterial />
-    </mesh>
+    <primitive object={scene} position={[0, 0, 0]} />
   )
 }
 
@@ -25,7 +24,11 @@ const App = () => {
   return (
     <div style={{ width: "100vw", height: "75vh" }}>
       <Canvas camera={{ position: [3, 1, 2] }}>
-        <Box position={[1, 1, 1]} name="A" />
+        <ambientLight intensity={2} />
+        <pointLight position={[40, 40, 40]} />
+        <Suspense fallback={null}>
+          <TheModel />
+        </Suspense>
         <Environment preset="forest" background />
         <OrbitControls />
         <axesHelper args={[5]} />
