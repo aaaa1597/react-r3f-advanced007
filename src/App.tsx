f764aef89742d42dd38309b98d9a6db312e667df
aaaa1597 <tokushi2011@gmail.com>
2024-01-07 11:25:31 +0900

    アニメーションも出来た。

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,56 @@
-import React, { useEffect, Suspense} from 'react';
+import React, { useEffect, Suspense, useRef, useState, useMemo } from 'react';
 import './App.css';
-import { Canvas, useLoader, useThree } from '@react-three/fiber'
+import { Canvas, useLoader, useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
-import { OrbitControls, Environment } from '@react-three/drei'
+import { OrbitControls, Environment, useFBX } from '@react-three/drei'
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 
 const FBXModel = () => {
+  /* FBXモデル読込み */
   const fbx = useLoader(FBXLoader, "./assets/Ch09_nonPBR.fbx");
+  /* AnimationClip(s)読込み */
+  const animCrips: THREE.AnimationClip[][] = []
+  animCrips[0] = useFBX('/assets/BreakdanceEnding2.fbx').animations
+  animCrips[1] = useFBX('/assets/BreakdanceUprockVar1.fbx').animations
+  animCrips[2] = useFBX('/assets/HipHopDancing.fbx').animations
+  animCrips[3] = useFBX('/assets/NorthernSoulSpin.fbx').animations
+  animCrips[4] = useFBX('/assets/SwingDancing.fbx').animations
+  animCrips[5] = useFBX('/assets/BreakdanceEnding1.fbx').animations
+  /* 変数定義 */
+  const mixer = useRef<THREE.AnimationMixer>();
+  const [ animIdx, setAnimIdx ] = useState<number>(0);
+  const animActions = useMemo(() => [] as THREE.AnimationAction[], [])
 
+  /* 初期化 */
   useEffect(() => {
     fbx.scale.multiplyScalar(0.02)
+    mixer.current = new THREE.AnimationMixer(fbx)
+    animCrips.forEach((val: THREE.AnimationClip[], idx: number) => {
+      animActions[idx] = mixer.current!.clipAction(val[0])
+    })
+    new Promise((resolve) => setTimeout((resolve) => {0}, 1000)).then(()=>animActions[0].play())
   }, [])
+
+  /* モーション切替え処理 */
+  useEffect(() => {
+    const act: THREE.AnimationAction = animActions[animIdx]
+    act?.reset().fadeIn(0.3).play()
+    return () => {
+      act?.fadeOut(0.3)
+    }
+  }, [animIdx])
+
+  /* FPS処理 */
+  useFrame((state, delta) => {
+    mixer.current!.update(delta);
+    const durationtime: number= animActions[animIdx].getClip().duration
+    const currenttime: number = animActions[animIdx].time
+    if(currenttime/durationtime > 0.9/*90%を超えたら次のモーションへ*/) {
+      const index: number = (animIdx+1) % (animCrips.length)
+      console.log('index=', index)
+      setAnimIdx( index )
+    }
+  });
 
   return (
     <primitive object={fbx} position={[1, -1, 1]} />
